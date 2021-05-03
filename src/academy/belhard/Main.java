7870d635f146333b2ac8e047540f5eeb78ee93ac
ekhrapan <yauheni.khrapan@gmail.com>
2021-05-03 21:17:39 +0300

    finish

--- src/academy/belhard/Main.java
+++ src/academy/belhard/Main.java
@@ -1,29 +1,38 @@
 package academy.belhard;
 
 import academy.belhard.db.joiner.DataJoiner;
+import academy.belhard.db.writer.SchoolDbWriter;
+import academy.belhard.db.writer.StudentsDbWriter;
 import academy.belhard.entity.JoinedDataItem;
+import academy.belhard.entity.School;
+import academy.belhard.entity.Student;
+import academy.belhard.io.IoConstants;
+import academy.belhard.io.reader.SchoolDataFileReader;
+import academy.belhard.io.reader.StudentDataFileReader;
 import academy.belhard.io.writer.JoinedDataFileWriter;
 
 import java.util.List;
 
+import static academy.belhard.io.IoConstants.*;
+
 public class Main {
 
     public static void main(String[] args) {
-//        SchoolDataFileReader dataFileReader = new SchoolDataFileReader("data\\schools.csv");
-//        List<School> schools = dataFileReader.read();
-//
-//        SchoolDbWriter.insert(schools);
+        SchoolDataFileReader dataFileReader = new SchoolDataFileReader(SCHOOLS_SOURCE_FILE);
+        List<School> schools = dataFileReader.read();
 
-//        StudentDataFileReader studentsReader = new StudentDataFileReader("data\\students.csv");
-//        List<Student> students = studentsReader.read();
-//
-//        StudentsDbWriter.insert(students);
+        SchoolDbWriter.insert(schools);
+
+        StudentDataFileReader studentsReader = new StudentDataFileReader(STUDENTS_SOURCE_FILE);
+        List<Student> students = studentsReader.read();
+
+        StudentsDbWriter.insert(students);
 
         List<JoinedDataItem> items  = DataJoiner.getJoinedData();
 
         System.out.println(items);
 
-        JoinedDataFileWriter dataFileWriter = new JoinedDataFileWriter("data\\result.csv");
+        JoinedDataFileWriter dataFileWriter = new JoinedDataFileWriter(RESULT_FILE);
         dataFileWriter.write(items);
     }
 }
--- src/academy/belhard/io/IoConstants.java
+++ src/academy/belhard/io/IoConstants.java
@@ -3,4 +3,8 @@ package academy.belhard.io;
 public class IoConstants {
 
     public static final String DELIMITER = ";";
+
+    public static final String SCHOOLS_SOURCE_FILE = "data\\schools.csv";
+    public static final String STUDENTS_SOURCE_FILE = "data\\students.csv";
+    public static final String RESULT_FILE = "data\\result.csv";
 }
